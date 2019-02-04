905467d32bab4509ee1b9673ab77b205057f20ac
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2019-02-04 22:05:37 +0000

    Convert PlayerPlatform Model to a Pivot

--- app/Models/PlayerPlatform.php
+++ app/Models/PlayerPlatform.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class PlayerPlatform extends Model
+class PlayerPlatform extends Pivot
 {
     //
 }
