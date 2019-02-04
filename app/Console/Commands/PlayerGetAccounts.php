734d0d2ea918bc9a6ace6fd86e258704392c9e31
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2019-02-04 21:40:21 +0000

    Create PlayerGetAccounts.php

---
+++ app/Console/Commands/PlayerGetAccounts.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Player;
+use Illuminate\Console\Command;
+
+class PlayerGetAccounts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'player:getaccounts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch the accounts of player\'s whose account ids have not been set.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Starting: Player - Get Accounts');
+    }
+}
