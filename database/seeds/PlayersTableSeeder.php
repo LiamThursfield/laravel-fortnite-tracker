58f019b75c1e8ad8dd6458ad631da62e61d5f5da
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2019-02-24 16:07:54 +0000

    PlayersTableSeeder - Add friends

--- database/seeds/PlayersTableSeeder.php
+++ database/seeds/PlayersTableSeeder.php
@@ -13,23 +13,14 @@ class PlayersTableSeeder extends Seeder
     {
         $now = \Carbon\Carbon::now();
 
-        // User with no platforms
-        DB::table('players')->insert([
-            'username' => 'JohnSmith',
-            'is_epic_account' => true,
-            'created_at' => $now
-        ]);
-
-        // User with all platforms
+        // Liam_Thursfield
         DB::table('players')->insert([
             'username' => 'Liam_Thursfield',
             'is_epic_account' => true,
             'created_at' => $now
         ]);
-
         $player = \App\Models\Player::whereUsername('Liam_Thursfield')->first();
         $platforms = \App\Models\Platform::all();
-
         if ($player && count($platforms)) {
             foreach ($platforms as $platform) {
                 DB::table('player_platforms')->insert([
@@ -40,13 +31,57 @@ class PlayersTableSeeder extends Seeder
             }
         }
 
-        // User with a platform but no epic account
+        // CoopsWasTaken
         DB::table('players')->insert([
-            'username' => 'coopswastaken',
+            'username' => 'CoopsWasTaken',
             'is_epic_account' => false
         ]);
+        $player = \App\Models\Player::whereUsername('CoopsWasTaken')->first();
+        $platform = \App\Models\Platform::find('playstation');
+        DB::table('player_platforms')->insert([
+            'player_id' => $player->id,
+            'platform_id' => $platform->id,
+            'created_at' => $now
+        ]);
 
-        $player = \App\Models\Player::whereUsername('coopswastaken')->first();
+        // oEZZAo
+        DB::table('players')->insert([
+            'username' => 'oEZZAo',
+            'is_epic_account' => true
+        ]);
+        $player = \App\Models\Player::whereUsername('oEZZAo')->first();
+        $platform = \App\Models\Platform::find('playstation');
+        DB::table('player_platforms')->insert([
+            'player_id' => $player->id,
+            'platform_id' => $platform->id,
+            'created_at' => $now
+        ]);
+        $platform = \App\Models\Platform::find('xbox');
+        DB::table('player_platforms')->insert([
+            'player_id' => $player->id,
+            'platform_id' => $platform->id,
+            'created_at' => $now
+        ]);
+
+        // CeeJaySteele
+        DB::table('players')->insert([
+            'username' => 'CeeJaySteele',
+            'is_epic_account' => true
+        ]);
+        $player = \App\Models\Player::whereUsername('CeeJaySteele')->first();
+        $platform = \App\Models\Platform::find('playstation');
+        DB::table('player_platforms')->insert([
+            'player_id' => $player->id,
+            'platform_id' => $platform->id,
+            'created_at' => $now
+        ]);
+
+        // Murfin94
+        DB::table('players')->insert([
+            'username' => 'Murfin94',
+            'is_epic_account' => true
+        ]);
+        $player = \App\Models\Player::whereUsername('Murfin94')->first();
         $platform = \App\Models\Platform::find('playstation');
         DB::table('player_platforms')->insert([
             'player_id' => $player->id,
