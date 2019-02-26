bb9950ffdb8cbfc5eed51331b248f9db3c0faaa6
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2019-02-26 21:15:33 +0000

    Add commands to kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\PlayerGetAccounts;
+use App\Console\Commands\PlayerGetStats;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        PlayerGetAccounts::class,
+        PlayerGetStats::class,
     ];
 
     /**
