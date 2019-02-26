24ef3d7ef6bfcb73bd8823ccf41e499ed114350e
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2019-02-26 12:49:34 +0000

    PlayerLifetimeStatsHelper - Add function to get all players cumulative lifetime stats

--- app/Helpers/PlayerLifetimeStatsHelper.php
+++ app/Helpers/PlayerLifetimeStatsHelper.php
@@ -3,13 +3,14 @@
 namespace App\Helpers;
 
 use App\ApiWrappers\TrackerNetwork\FortniteTracker\Models\LifetimeStats;
+use App\Models\Player;
 use App\Models\PlayerLifetimeStats;
 use Carbon\Carbon;
 
 class PlayerLifetimeStatsHelper {
 
     /**
-     * Create/Update the Player Lifetime stats
+     * Create/Update the Player Lifetime stats for all players
      * using the LifetimeStats fetched via the API
      * @param int|null $player_id
      * @param string|null $platform_id
@@ -43,6 +44,48 @@ class PlayerLifetimeStatsHelper {
         $player_lifetime_stats->save();
 
         return true;
+    }
+
+    /**
+     * Get the cumulative lifetime stats
+     * @param int $per_page
+     * @param int $page
+     * @return array
+     */
+    public static function getAllPlayersCumulative($per_page = 10, $page = 1) {
+        $players = Player::whereNotNull('account_id')
+            ->with('lifetime_stats')
+            ->orderBy('username', 'asc')
+            ->paginate($per_page, ['*'], 'page', $page);
+
+        $player_stats = [];
+        foreach ($players as $player) {
+            $matches = 0;
+            $wins = 0;
+            $kills = 0;
+            $deaths = 0;
+
+            foreach ($player->lifetime_stats as $lifetime_stats) {
+                $matches += $lifetime_stats->matches_played;
+                $kills += $lifetime_stats->kills;
+                $wins += $lifetime_stats->top_1;
+                $deaths += ($lifetime_stats->kills / $lifetime_stats->kd);
+            }
+            unset($lifetime_stats);
+
+            $kd = number_format($kills / $deaths, 2);
+            $deaths = round($deaths);
+
+            $player_stats[$player->username] = [
+                'matches_played' => $matches,
+                'top_1' => $wins,
+                'kills' => $kills,
+                'deaths' => $deaths,
+                'kd' => $kd,
+            ];
+        }
+
+        return $player_stats;
     }
 
 }
