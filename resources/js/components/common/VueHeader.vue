45e9d797b823a3767fd6ac441235142a04d6b40a
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2019-03-01 20:00:24 +0000

    Update platform and season on mouse down

--- resources/js/components/common/VueHeader.vue
+++ resources/js/components/common/VueHeader.vue
@@ -14,23 +14,26 @@
 
                 <ul class="header-platform-season-nav">
                     <li class="platform-season-nav-item season-nav-item">
-                        <div class="nav-selected"><span>Lifetime</span> <i class="fas fa-sort-down"></i></div>
+                        <div class="nav-selected"><span>{{ selected_season }}</span> <i class="fas fa-sort-down"></i></div>
                         <div class="nav-dropdown">
                             <ul>
-                                <li class="nav-item">Lifetime</li>
-                                <li class="nav-item">Season 7</li>
-                                <li class="nav-item">Season 8</li>
+                                <li class="nav-item" v-for="season in seasons"
+                                    @mousedown="selected_season(season)">
+                                    {{ season }}
+                                </li>
+
                             </ul>
                         </div>
                     </li>
 
                     <li class="platform-season-nav-item platform-nav-item">
-                        <div class="nav-selected"><span>Playstation</span> <i class="fas fa-sort-down"></i></div>
+                        <div class="nav-selected"><span>{{ selected_platform_name }}</span> <i class="fas fa-sort-down"></i></div>
                         <div class="nav-dropdown">
                             <ul>
-                                <li class="nav-item">PC</li>
-                                <li class="nav-item">Playstation</li>
-                                <li class="nav-item">Xbox</li>
+                                <li class="nav-item" v-for="(platform, platform_id) in platforms"
+                                    @mousedown="selectPlatform(platform_id)">
+                                    {{ platform }}
+                                </li>
                             </ul>
                         </div>
                     </li>
@@ -46,6 +49,13 @@
         props: {
             platforms: Object,
             seasons: Array
+        },
+        data() {
+            return {
+                selected_platform_id: '',
+                selected_platform_name: '',
+                selected_season: ''
+            }
         },
         computed: {
             activePath: function () {
@@ -64,8 +74,19 @@
                 return path_array[1];
             }
         },
+        methods: {
+            selectPlatform: function(platform_id) {
+                console.log('select platform', platform_id);
+                this.selected_platform_id = platform_id;
+                this.selected_platform_name = this.platforms[platform_id];
+            },
+            selectSeason: function(season) {
+                this.selected_season = season;
+            }
+        },
         mounted() {
-            console.log(window.location.pathname);
+            this.selected_season = this.seasons[0];
+            this.selectPlatform(Object.keys(this.platforms)[0]);
         }
     }
 </script>
