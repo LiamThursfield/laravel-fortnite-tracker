7a1c1a08915966a72a7e62b1147f3bcf0cf0a3b8
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2019-03-02 10:49:50 +0000

    Move platform season logic to the app instance

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,5 +14,90 @@ Vue.component('vue-header', require('./components/common/VueHeader.vue').default
  * Instantiate the Vue application
  */
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data() {
+        return {
+            loaded_platform_seasons: false,
+
+            // Platforms Data
+            platforms: {},
+            local_storage_platform_id: 'selected_platform_id',
+            selected_platform_id: '',
+            selected_platform_name: '',
+            default_platforms: {
+                combined: 'Combined',
+                pc: 'PC',
+                playstation: 'Playstation',
+                xbox: 'Xbox'
+            },
+
+            // Seasons Data
+            seasons: [],
+            local_storage_season: 'selected_season',
+            selected_season: '',
+            default_seasons: [
+                'Lifetime', 'Season 8', 'Season 7'
+            ]
+        }
+    },
+    methods: {
+        // API Calls
+        fetchPlatformSeasons: function() {
+            let url = '/api/v1/platform_seasons?with_combined_platforms=true';
+            axios
+                .get(url)
+                .then(response => {
+                    if (response.data && response.data.platforms && response.data.seasons) {
+                        this.platforms = response.data.platforms;
+                        this.seasons = response.data.seasons;
+                    } else {
+                        this.platforms = this.default_platforms;
+                        this.seasons = this.default_seasons;
+                    }
+
+                    this.getSelectedPlatform();
+                    this.getSelectedSeason();
+                    this.loaded_platform_seasons = true;
+                })
+                .catch(error => {
+                    // TODO, error notifications
+                    this.platforms = this.default_platforms;
+                    this.seasons = this.default_seasons;
+                    this.getSelectedPlatform();
+                    this.getSelectedSeason();
+                    this.loaded_platform_seasons = true;
+                })
+        },
+
+        // Platform Methods
+        getSelectedPlatform: function() {
+            if (localStorage.getItem(this.local_storage_platform_id)) {
+                this.selectPlatform(localStorage.getItem(this.local_storage_platform_id));
+            } else {
+                this.selectPlatform(Object.keys(this.platforms)[0]);
+                this.selected_season = this.seasons[0];
+            }
+        },
+        selectPlatform: function(platform_id) {
+            this.selected_platform_id = platform_id;
+            this.selected_platform_name = this.platforms[platform_id];
+            localStorage.setItem(this.local_storage_platform_id, platform_id);
+        },
+
+        // Season Methods
+        getSelectedSeason: function() {
+            if (localStorage.getItem(this.local_storage_season)) {
+                this.selected_season = localStorage.getItem(this.local_storage_season);
+            } else {
+                this.selected_season = this.seasons[0];
+            }
+        },
+        selectSeason: function(season) {
+            this.selected_season = season;
+            localStorage.setItem(this.local_storage_season, season);
+        }
+    },
+    created() {
+        this.fetchPlatformSeasons();
+    }
 });
--- resources/js/components/common/VueHeader.vue
+++ resources/js/components/common/VueHeader.vue
@@ -12,13 +12,13 @@
                     <li><a class="nav-item" :class="{ 'nav-item-active': activePath === 'squads'}" href="/squads">Squads</a></li>
                 </ul>
 
-                <ul class="header-platform-season-nav">
+                <ul class="header-platform-season-nav" v-if="$root.$data.loaded_platform_seasons">
                     <li class="platform-season-nav-item season-nav-item"
                     @mouseover="show_seasons = true" @mouseleave="show_seasons = false">
-                        <div class="nav-selected"><span>{{ selected_season }}</span> <i class="fas fa-sort-down"></i></div>
+                        <div class="nav-selected"><span>{{ $root.$data.selected_season }}</span> <i class="fas fa-sort-down"></i></div>
                         <div class="nav-dropdown" :class="{show:show_seasons}">
                             <ul>
-                                <li class="nav-item" v-for="season in seasons"
+                                <li class="nav-item" v-for="season in $root.$data.seasons"
                                     @mousedown="selectSeason(season)">
                                     {{ season }}
                                 </li>
@@ -29,10 +29,10 @@
 
                     <li class="platform-season-nav-item platform-nav-item"
                         @mouseover="show_platforms = true" @mouseleave="show_platforms = false">
-                        <div class="nav-selected"><span>{{ selected_platform_name }}</span> <i class="fas fa-sort-down"></i></div>
+                        <div class="nav-selected"><span>{{ $root.$data.selected_platform_name }}</span> <i class="fas fa-sort-down"></i></div>
                         <div class="nav-dropdown" :class="{show:show_platforms}">
                             <ul>
-                                <li class="nav-item" v-for="(platform, platform_id) in platforms"
+                                <li class="nav-item" v-for="(platform, platform_id) in $root.$data.platforms"
                                     @mousedown="selectPlatform(platform_id)">
                                     {{ platform }}
                                 </li>
@@ -55,13 +55,8 @@
         data() {
             return {
                 // Platform Data
-                local_storage_platform_id: 'selected_platform_id',
-                selected_platform_id: '',
-                selected_platform_name: '',
                 show_platforms: false,
                 // Season Data
-                local_storage_season: 'selected_season',
-                selected_season: '',
                 show_seasons: false,
             }
         },
@@ -83,39 +78,15 @@
             }
         },
         methods: {
-            // Platform Methods
-            getSelectedPlatform: function() {
-                if (localStorage.getItem(this.local_storage_platform_id)) {
-                    this.selectPlatform(localStorage.getItem(this.local_storage_platform_id));
-                } else {
-                    this.selectPlatform(Object.keys(this.platforms)[0]);
-                    this.selected_season = this.seasons[0];
-                }
-            },
             selectPlatform: function(platform_id) {
-                this.selected_platform_id = platform_id;
-                this.selected_platform_name = this.platforms[platform_id];
-                localStorage.setItem(this.local_storage_platform_id, platform_id);
+                this.$root.selectPlatform(platform_id);
                 this.show_platforms = false;
             },
-
-            // Season Methods
-            getSelectedSeason: function() {
-                if (localStorage.getItem(this.local_storage_season)) {
-                    this.selected_season = localStorage.getItem(this.local_storage_season);
-                } else {
-                    this.selected_season = this.seasons[0];
-                }
-            },
             selectSeason: function(season) {
-                this.selected_season = season;
-                localStorage.setItem(this.local_storage_season, season);
+                this.$root.selectSeason(season);
                 this.show_seasons = false;
             }
         },
-        mounted() {
-            this.getSelectedPlatform();
-            this.getSelectedSeason();
-        }
+        mounted() {}
     }
 </script>
